acf082232fbf162111cae69e0b54186ed1deb02a
jiejiang <mail.jie.jiang@gmail.com>
2016-08-22 11:35:07 +0100

    modifies

--- app/main/postorder.py
+++ app/main/postorder.py
@@ -64,110 +64,118 @@ PROVINCE_INFO_MAP = {
     u"新疆": {'ticket_initial': 1, 'package_type': u"标准快递"},
 }
 
-ITEM_NAME_RE = re.compile(ur"^.*?((([123一二三])|([4四]))段|(\d+)g)$", flags=re.U | re.I)
+ITEM_NAME_RE = re.compile(
+    ur"^.*?((([123一二三])|([4四]))段|(\d+)g)$", flags=re.U | re.I)
 
 ITEM_NAME_MAP_INFO = {
-    u"爱他美1段" : {
+    u"爱他美1段": {
         "net_weight": 0.9,
-        "gross_weights" : {
+        "gross_weights": {
             4: 4.35,
             6: 7,
         },
-        "price_per_kg" : 89.06,
-        "full_name" : u"爱他美奶粉1段900g",
+        "price_per_kg": 89.06,
+        "full_name": u"爱他美奶粉1段900g",
     },
-    u"爱他美2段" : {
+    u"爱他美2段": {
         "net_weight": 0.9,
-        "gross_weights" : {
+        "gross_weights": {
             4: 4.35,
             6: 7,
         },
-        "price_per_kg" : 89.06,
-        "full_name" : u"爱他美奶粉2段900g",
+        "price_per_kg": 89.06,
+        "full_name": u"爱他美奶粉2段900g",
     },
-    u"爱他美3段" : {
+    u"爱他美3段": {
         "net_weight": 0.9,
-        "gross_weights" : {
+        "gross_weights": {
             4: 4.35,
             6: 7,
         },
-        "price_per_kg" : 89.06,
-        "full_name" : u"爱他美奶粉3段900g",
+        "price_per_kg": 89.06,
+        "full_name": u"爱他美奶粉3段900g",
     },
-    u"爱他美4段" : {
+    u"爱他美4段": {
         "net_weight": 0.8,
-        "gross_weights" : {
+        "gross_weights": {
             4: 3.90,
             6: 6,
         },
-        "price_per_kg" : 86.50,
-        "full_name" : u"爱他美奶粉4段800g",
+        "price_per_kg": 86.50,
+        "full_name": u"爱他美奶粉4段800g",
     },
-    u"牛栏1段" : {
+    u"牛栏1段": {
         "net_weight": 0.9,
-        "gross_weights" : {
+        "gross_weights": {
             4: 4.35,
             6: 7,
         },
-        "price_per_kg" : 89.06,
-        "full_name" : u"牛栏奶粉1段900g",
+        "price_per_kg": 89.06,
+        "full_name": u"牛栏奶粉1段900g",
     },
-    u"牛栏2段" : {
+    u"牛栏2段": {
         "net_weight": 0.9,
-        "gross_weights" : {
+        "gross_weights": {
             4: 4.35,
             6: 7,
         },
-        "price_per_kg" : 86.50,
-        "full_name" : u"牛栏奶粉2段900g",
+        "price_per_kg": 86.50,
+        "full_name": u"牛栏奶粉2段900g",
     },
-    u"牛栏3段" : {
+    u"牛栏3段": {
         "net_weight": 0.9,
-        "gross_weights" : {
+        "gross_weights": {
             4: 4.35,
             6: 7,
         },
-        "price_per_kg" : 86.50,
-        "full_name" : u"牛栏奶粉3段900g",
+        "price_per_kg": 86.50,
+        "full_name": u"牛栏奶粉3段900g",
     },
-    u"牛栏4段" : {
+    u"牛栏4段": {
         "net_weight": 0.8,
-        "gross_weights" : {
+        "gross_weights": {
             4: 3.90,
             6: 6,
         },
-        "price_per_kg" : 94.30,
-        "full_name" : u"牛栏奶粉4段800g",
+        "price_per_kg": 94.30,
+        "full_name": u"牛栏奶粉4段800g",
     },
 }
+
 
 def calculate_item_info(n_row, item_name, item_count):
     item_name = "".join(item_name.strip().split()).decode("utf8")
     if not item_name in ITEM_NAME_MAP_INFO:
-        raise Exception, u"第%d行包含未注册商品名称: %s" % (n_row+1, item_name)
+        raise Exception, u"第%d行包含未注册商品名称: %s" % (n_row + 1, item_name)
     info = ITEM_NAME_MAP_INFO[item_name]
     if not item_count in info["gross_weights"]:
-        raise Exception, u"第%d行商品[%s]包含未注册数量:%d" % (n_row+1, item_name, item_count)
+        raise Exception, u"第%d行商品[%s]包含未注册数量:%d" % (
+            n_row + 1, item_name, item_count)
     return info["net_weight"] * item_count * info["price_per_kg"], info["net_weight"] * item_count, \
-           info["gross_weights"][item_count], info["price_per_kg"], \
-           info["full_name"] if "full_name" in info else item_name
+        info["gross_weights"][item_count], info["price_per_kg"], \
+        info["full_name"] if "full_name" in info else item_name
+
 
 def calculate_item_info_from_db(n_row, item_name, item_count):
     item_name = "".join(item_name.strip().split()).decode("utf8")
     search_result = ProductInfo.find_product_and_weight(item_name, item_count)
     if not search_result:
-        raise Exception, u"第%d行包含未注册商品名称和箱件数 %s[%d件]" % (n_row+1, item_name, item_count)
+        raise Exception, u"第%d行包含未注册商品名称和箱件数 %s[%d件]" % (
+            n_row + 1, item_name, item_count)
     product_info, gross_weight_per_box = search_result
     return product_info.net_weight * item_count * product_info.price_per_kg, product_info.net_weight * item_count, \
-           gross_weight_per_box, product_info.price_per_kg, \
-           product_info.full_name if product_info.full_name else item_name
+        gross_weight_per_box, product_info.price_per_kg, \
+        product_info.full_name if product_info.full_name else item_name
+
 
 class NoTextImageWriter(ImageWriter):
+
     def __init__(self):
         super(NoTextImageWriter, self).__init__()
 
     def _paint_text(self, xpos, ypos):
         pass
+
 
 def generate_pdf(ticket_number, filename, context, tmpdir):
     bot_image = os.path.join(tmpdir, 'bot_barcode_trim.png')
@@ -222,15 +230,20 @@ def fetch_ticket_number(n_row, receiver_city):
     city_name = "".join(receiver_city.strip().split())
     cities = City.find_province_path(city_name)
     if not cities:
-        raise Exception, "Cannot find province: %s at row %d" % (city_name, n_row)
+        raise Exception, "Cannot find province: %s at row %d" % (
+            city_name, n_row)
     if not cities[0].name in PROVINCE_INFO_MAP:
-        raise Exception, "Post to province %s is not supported: %s at row %d" % (city_name, n_row)
+        raise Exception, "Post to province %s is not supported: %s at row %d" % (
+            city_name, n_row)
     info = PROVINCE_INFO_MAP[cities[0].name]
     order = Order.pick_first(info['ticket_initial'])
     if order is None:
-        raise Exception, u"订单号不足, 订单类型:%s" % Order.Type.types[info['ticket_initial']]
-    province_name, municipal_name, address_header = City.normalize_province_path(cities)
+        raise Exception, u"订单号不足, 订单类型:%s" % Order.Type.types[
+            info['ticket_initial']]
+    province_name, municipal_name, address_header = City.normalize_province_path(
+        cities)
     return info['package_type'], order, province_name, municipal_name, address_header
+
 
 def process_row(n_row, in_row, barcode_dir, tmpdir, job=None):
     p_data = []
@@ -263,18 +276,22 @@ def process_row(n_row, in_row, barcode_dir, tmpdir, job=None):
     receiver_address = receiver_address_header + receiver_address
 
     pc_text = receiver_province + receiver_municipal
-    receiver_province_city_font_size = "3" if len(pc_text) <= 10 else "2.5" if len(pc_text) <= 15 else "2"
+    receiver_province_city_font_size = "3" if len(
+        pc_text) <= 10 else "2.5" if len(pc_text) <= 15 else "2"
 
     order.used = True
     order.used_time = datetime.datetime.utcnow()
-    order.sender_address = ", ".join((sender_name, sender_address, sender_phone))
-    order.receiver_address = ", ".join((receiver_address, receiver_city, receiver_post_code, receiver_mobile))
+    order.sender_address = ", ".join(
+        (sender_name, sender_address, sender_phone))
+    order.receiver_address = ", ".join(
+        (receiver_address, receiver_city, receiver_post_code, receiver_mobile))
     order.receiver_id_number = id_number
     order.receiver_name = receiver_name
     if job:
         order.job = job
     ticket_number = order.order_number
-    full_address = "".join(filter(lambda x:x.strip(), (receiver_province, receiver_city, receiver_address)))
+    full_address = "".join(filter(
+        lambda x: x.strip(), (receiver_province, receiver_city, receiver_address)))
 
     total_price = 0
     item_names = []
@@ -285,7 +302,7 @@ def process_row(n_row, in_row, barcode_dir, tmpdir, job=None):
         unit_price = in_row[u'物品单价（英镑）%s' % suffix]
 
         if item_name is None or pd.isnull(item_name):
-            raise Exception, u"第%d行第%d个商品名称为空" % (n_row, i+1)
+            raise Exception, u"第%d行第%d个商品名称为空" % (n_row, i + 1)
         item_name = str(item_name).strip()
 
         # m = ITEM_NAME_RE.match(item_name)
@@ -308,8 +325,6 @@ def process_row(n_row, in_row, barcode_dir, tmpdir, job=None):
         item_names.append(item_full_name)
         total_price += sub_total_price
 
-
-
         p_data.append([
             ticket_number, sender_name, sender_address, sender_phone, receiver_name, receiver_mobile, receiver_city if receiver_city else receiver_province,
             receiver_post_code, full_address, item_full_name, item_count, sub_total_price, gross_weight, item_full_name,
@@ -328,7 +343,8 @@ def process_row(n_row, in_row, barcode_dir, tmpdir, job=None):
 
     item_names = ", ".join(item_names)
 
-    generate_pdf(ticket_number, os.path.join(barcode_dir, '%s.pdf' % ticket_number), locals(), tmpdir)
+    generate_pdf(ticket_number, os.path.join(
+        barcode_dir, '%s.pdf' % ticket_number), locals(), tmpdir)
 
     return ticket_number, pd.DataFrame(p_data, columns=[
         u'快件单号', u'发件人', u'发件人地址', u'电话号码', u'收件人', u'电话号码.1', u'城市',
@@ -350,12 +366,12 @@ def xls_to_orders(input, output, tmpdir, percent_callback=None, job=None):
     if percent_callback:
         percent_callback(0)
     in_df = pd.read_excel(input, converters={
-        u'发件人电话号码': lambda x:str(x),
-        u'收件人邮编' : lambda x:str(x),
-        u'收件人手机号\n（11位数）' : lambda x:str(x),
-        u'身份证号\n(EMS需要)' : lambda x:str(x),
-        u'收件人手机号（11位数）' : lambda x:str(x),
-        u'身份证号(EMS需要)' : lambda x:str(x),
+        u'发件人电话号码': lambda x: str(x),
+        u'收件人邮编': lambda x: str(x),
+        u'收件人手机号\n（11位数）': lambda x: str(x),
+        u'身份证号\n(EMS需要)': lambda x: str(x),
+        u'收件人手机号（11位数）': lambda x: str(x),
+        u'身份证号(EMS需要)': lambda x: str(x),
     })
     normalize_columns(in_df)
 
@@ -386,7 +402,8 @@ def xls_to_orders(input, output, tmpdir, percent_callback=None, job=None):
 
     ticket_numbers = []
     for index, in_row in in_df.iterrows():
-        ticket_number, p_data, c_data = process_row(index, in_row, barcode_dir, tmpdir, job)
+        ticket_number, p_data, c_data = process_row(
+            index, in_row, barcode_dir, tmpdir, job)
         ticket_numbers.append(ticket_number)
         package_data.append(p_data)
         customs_data.append(c_data)
@@ -399,7 +416,8 @@ def xls_to_orders(input, output, tmpdir, percent_callback=None, job=None):
         if not os.path.exists(pdf_file):
             raise Exception, "Failed to generate pdf: %s" % ticket_number
         merger.append(PdfFileReader(file(pdf_file, 'rb')))
-    merger.write(os.path.join(output, u"面单_%d页.pdf".encode('utf8') % len(ticket_numbers)))
+    merger.write(os.path.join(
+        output, u"面单_%d页.pdf".encode('utf8') % len(ticket_numbers)))
     shutil.rmtree(barcode_dir)
 
     package_final_df = pd.concat(package_data, ignore_index=True)
@@ -420,7 +438,7 @@ def xls_to_orders(input, output, tmpdir, percent_callback=None, job=None):
     customs_final_df[u'包装种类'] = '4'
     customs_final_df[u'保价费'] = 0
     customs_final_df[u'保价费币制'] = '142'
-    customs_final_df[u'进出口岸代码  商品实际进出我国关境口岸海关的关区代码'] = '6813'
+    customs_final_df[u'进出口岸代码  商品实际进出我国关境口岸海关的关区代码'] = '6841'
     customs_final_df[u'收件人所在国家(地区）代码'] = '142'
     customs_final_df[u'收件人证件类型'] = '1'
     customs_final_df[u'发货人所在国家(地区）代码'] = '303'
@@ -432,7 +450,7 @@ def xls_to_orders(input, output, tmpdir, percent_callback=None, job=None):
     customs_final_df[u'商品备案号'] = '01010700'
     customs_final_df[u'商品币制'] = '142'
     customs_final_df[u'进出口标识'] = 'I'
-    customs_final_df[u'码头/货场代码（为物流监控备用）'] = '6813'
+    customs_final_df[u'码头/货场代码（为物流监控备用）'] = '6841'
     customs_final_df[u'仓单申报类型N表示新增M修改'] = 'N'
 
     customs_final_df.to_excel(os.path.join(output, u"江门申报单.xlsx".encode('utf8')),
@@ -441,20 +459,22 @@ def xls_to_orders(input, output, tmpdir, percent_callback=None, job=None):
     if percent_callback:
         percent_callback(100)
 
+
 def read_order_numbers(inxlsx):
     df = pd.read_excel(inxlsx, converters={
-        u"提取单号" : lambda x:str(x)
+        u"提取单号": lambda x: str(x)
     })
     if not u"提取单号" in df:
         raise Exception, u"输入Excel格式错误"
     order_numbers = df[u"提取单号"]
     return order_numbers
 
+
 def retract_from_order_numbers(download_folder, order_numbers, output, retraction=None):
     if not os.path.exists(output):
         os.makedirs(output)
 
-    #find all jobs and job to order number map
+    # find all jobs and job to order number map
     receiver_sig_to_order_numbers = {}
     all_order_numbers = set()
     uuid_to_order_numbers = {}
@@ -466,19 +486,21 @@ def retract_from_order_numbers(download_folder, order_numbers, output, retractio
             all_order_numbers.add(order_number)
         order = Order.find_by_order_number(order_number)
         if not order:
-            raise Exception, u"第%d行包含未上载订单号: %s" % (i+1, order_number)
+            raise Exception, u"第%d行包含未上载订单号: %s" % (i + 1, order_number)
         if not order.used:
-            raise Exception, u"第%d行包含未使用订单号: %s" % (i+1, order_number)
+            raise Exception, u"第%d行包含未使用订单号: %s" % (i + 1, order_number)
         if order.retraction:
             raise Exception, u"第%d行订单号已被提取: %s, 提取信息为: Uuid [%s], 时间 [%s]" % \
-                             (i+1, order_number, order.retraction.uuid, time_to_filename(order.retraction.timestamp))
+                             (i + 1, order_number, order.retraction.uuid,
+                              time_to_filename(order.retraction.timestamp))
         receiver_sig = order.receiver_name
         if not receiver_sig in receiver_sig_to_order_numbers:
             receiver_sig_to_order_numbers[receiver_sig] = []
         if len(receiver_sig_to_order_numbers[receiver_sig]) >= 2:
             raise Exception, u"单个收件人超过最大订单数: 第%d行订单(%s)与[ %s ]包含相同收件人(%s)" % \
-                             (i+1, order_number,
-                              " / ".join(["第%d行订单(%s)" % (x+1, y) for x, y in receiver_sig_to_order_numbers[receiver_sig]]),
+                             (i + 1, order_number,
+                              " / ".join(["第%d行订单(%s)" % (x + 1, y)
+                                          for x, y in receiver_sig_to_order_numbers[receiver_sig]]),
                               receiver_sig)
         receiver_sig_to_order_numbers[receiver_sig].append((i, order_number))
         uuid = str(order.job.uuid)
@@ -496,36 +518,40 @@ def retract_from_order_numbers(download_folder, order_numbers, output, retractio
             raise Exception, u"历史数据丢失:%s" % uuid
         with zipfile.ZipFile(job_file) as z:
             package_df = pd.read_excel(z.open(u"机场报关单.xlsx"), index_col=0, converters={
-                u'快件单号': lambda x:str(x),
-                u'电话号码' : lambda x:str(x),
-                u'电话号码.1' : lambda x:str(x),
-                u'邮编' : lambda x:str(x),
-                u'税号' : lambda x:str(x),
-                u'备注' : lambda x:str(x),
+                u'快件单号': lambda x: str(x),
+                u'电话号码': lambda x: str(x),
+                u'电话号码.1': lambda x: str(x),
+                u'邮编': lambda x: str(x),
+                u'税号': lambda x: str(x),
+                u'备注': lambda x: str(x),
             })
             customs_df = pd.read_excel(z.open(u"江门申报单.xlsx"), converters={
-                u"企业运单编号" : lambda x:str(x),
-                u"收件人省市区代码" : lambda x:str(x),
-                u"收件人电话" : lambda x:str(x),
-                u"收件人证件号码" : lambda x:str(x),
-                u"发货人省市区代码" : lambda x:str(x),
-                u"发货人电话" : lambda x:str(x),
-                u"商品备案号" : lambda x:str(x),
-                u"发货人电话" : lambda x:str(x),
-                u'计量单位': lambda x:str(x),
+                u"企业运单编号": lambda x: str(x),
+                u"收件人省市区代码": lambda x: str(x),
+                u"收件人电话": lambda x: str(x),
+                u"收件人证件号码": lambda x: str(x),
+                u"发货人省市区代码": lambda x: str(x),
+                u"发货人电话": lambda x: str(x),
+                u"商品备案号": lambda x: str(x),
+                u"发货人电话": lambda x: str(x),
+                u'计量单位': lambda x: str(x),
             })
-            sub_package_df = package_df[package_df[u"快件单号"].isin(order_number_set)]
-            sub_customs_df = customs_df[customs_df[u"企业运单编号"].isin(order_number_set)]
+            sub_package_df = package_df[
+                package_df[u"快件单号"].isin(order_number_set)]
+            sub_customs_df = customs_df[
+                customs_df[u"企业运单编号"].isin(order_number_set)]
             package_dfs.append(sub_package_df)
             customs_dfs.append(sub_customs_df)
 
     package_final_df = pd.concat(package_dfs, ignore_index=True)
     package_final_df.index += 1
-    package_final_df.to_excel(os.path.join(output, u"机场报关单.xlsx".encode('utf8')), index_label="NO")
+    package_final_df.to_excel(os.path.join(
+        output, u"机场报关单.xlsx".encode('utf8')), index_label="NO")
     customs_final_df = pd.concat(customs_dfs, ignore_index=True)
     customs_final_df[u'订单编号'] = None
     customs_final_df[u'商品货号'] = range(1, len(customs_final_df.index) + 1)
-    customs_final_df.to_excel(os.path.join(output, u"江门申报单.xlsx".encode('utf8')), index=False)
+    customs_final_df.to_excel(os.path.join(
+        output, u"江门申报单.xlsx".encode('utf8')), index=False)
 
 if __name__ == "__main__":
     parser = OptionParser()
